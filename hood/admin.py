02128db16fc90885e58c14b2f3ae4f8f20ecb403
Munyiwamwangi <josseymunyi@gmail.com>
2019-09-14 15:23:38 +0300

    add hood app urls in admin, next is registerng the models

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.conf.urls import url,include
+from django.contrib.auth import views
 
 # Register your models here.
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^',include('hood.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
+]
