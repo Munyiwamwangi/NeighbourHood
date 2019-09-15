8e09e433a6b12a635df7cd1e1f939771f14f1b78
Munyiwamwangi <josseymunyi@gmail.com>
2019-09-15 14:25:01 +0300

    adding user post html

--- hood/urls.py
+++ hood/urls.py
@@ -6,7 +6,7 @@ from .views import (PostListView, PostDetailView, PostCreateView, PostUpdateView
 
 urlpatterns = [
     path('',views.home, name='home'), #for neighboorhoods
-    path('',views.posting, name='posting'), #for posts
+    path('',views.posting, name='blogging'), #for posts
 
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
