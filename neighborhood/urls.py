fd5b53b4425ad29241d2dd622fe18a64306ecc62
Munyiwamwangi <josseymunyi@gmail.com>
2019-09-15 12:20:40 +0300

    am shifting from django 1.11.24 to django 2.2.5 in this branch because i want to use the generic view function offered by django, its a practice branch, i hope it works out

--- neighborhood/urls.py
+++ neighborhood/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.conf.urls import url,include
 from django.contrib import admin
 from django.contrib.auth import views
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from users import views as user_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
