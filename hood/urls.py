135983df3a28612d78d3229ed1367ed1f2a59d6a
Munyiwamwangi <josseymunyi@gmail.com>
2019-09-15 18:22:17 +0300

    redirection after logn in fixed, using LOGIN_REDIRECT_URL to home, app working perfect, proceeding to adding businesses

--- hood/urls.py
+++ hood/urls.py
@@ -7,9 +7,9 @@ from .views import (PostListView, PostDetailView, PostCreateView, PostUpdateView
 
 
 urlpatterns = [
-    path('',views.home, name='home'), #for neighboorhoods
-    # path('posts/', PostListView.as_view(), name='blogging'),
-	path('search/', views.search_results, name='search_results'),
+    path('',views.home, name='home'),
+    path('blogging', PostListView.as_view(), name='blogging'),
+    path('search/', views.search_results, name='search_results'),
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
@@ -17,7 +17,3 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='about'),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -144,8 +144,7 @@ USE_L10N = True
 USE_TZ = True
 
 LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'posts'
-
+LOGIN_REDIRECT_URL = ('..')#user is taken home after login in
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
