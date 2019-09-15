431119baae56efc094c81a884c73d1b8fb80f569
Munyiwamwangi <josseymunyi@gmail.com>
2019-09-15 15:53:00 +0300

    had some migration errors coz i forgot to migrate app users, but now solved, testing app

--- hood/urls.py
+++ hood/urls.py
@@ -8,8 +8,8 @@ from .views import (PostListView, PostDetailView, PostCreateView, PostUpdateView
 
 urlpatterns = [
     path('',views.home, name='home'), #for neighboorhoods
-    path('',views.blogging, name='blogging'), #for posts
-
+    path('blogging/',views.blogging, name='blogging'), #for posts
+	path('search/', views.search_results, name='search_results'),
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
